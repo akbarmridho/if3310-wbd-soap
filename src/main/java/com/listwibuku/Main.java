1efe7e9d4ca3b1d0a6e1fefcbe14e001f4b3eba8
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2023-11-03 16:07:23 +0700

    feat: middleware template

--- src/main/java/com/listwibuku/Main.java
+++ src/main/java/com/listwibuku/Main.java
@@ -1,9 +1,13 @@
 package com.listwibuku;
 
+import com.listwibuku.middleware.Authentication;
+import com.listwibuku.middleware.Logger;
 import com.listwibuku.services.SubscriberImpl;
 import com.listwibuku.utils.Config;
 
 import javax.xml.ws.Endpoint;
+import javax.xml.ws.handler.Handler;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,8 +16,17 @@ public class Main {
             String host = instance.getHost();
             Integer port = instance.getPort();
 
-            System.out.println("Starting server at " + host + ":" + port);
-            Endpoint.publish(host + ":" + port + "/subscriberservice", new SubscriberImpl());
+            Endpoint endpoint = Endpoint.publish(host + ":" + port + "/subscriberservice", new SubscriberImpl());
+            List<Handler> handlerChain = endpoint.getBinding().getHandlerChain();
+
+            // setup api middleware
+            handlerChain.add(new Authentication());
+
+            // setup logger middleware
+            handlerChain.add(new Logger());
+
+            endpoint.getBinding().setHandlerChain(handlerChain);
+
             System.out.println("Server started at " + host + ":" + port);
         } catch (Exception e) {
             e.printStackTrace();
---
+++ src/main/java/com/listwibuku/middleware/Logger.java
@@ -0,0 +1,29 @@
+package com.listwibuku.middleware;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.handler.soap.SOAPHandler;
+import javax.xml.ws.handler.soap.SOAPMessageContext;
+import java.util.Set;
+
+public class Logger implements SOAPHandler<SOAPMessageContext> {
+    @Override
+    public Set<QName> getHeaders() {
+        return null;
+    }
+
+    @Override
+    public boolean handleMessage(SOAPMessageContext context) {
+        return true;
+    }
+
+    @Override
+    public boolean handleFault(SOAPMessageContext context) {
+        return true;
+    }
+
+    @Override
+    public void close(MessageContext context) {
+
+    }
+}
